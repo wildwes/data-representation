84a4d52fc7a2bdc9805f693b315533f8caa58b69
Wes Waters <weswaters@rocketmail.com>
2020-04-28 10:12:37 -0500

    finished

---
+++ data-rep.js
@@ -0,0 +1,39 @@
+// Take the following data and represent it in
+
+// A string
+// An object
+// An array
+
+// "lunch"
+// "peanut butter and jelly"
+// "whole wheat"
+// "grape juice"
+// "apple"
+
+
+// strings
+const lunch = "lunch";
+
+const sandwhich = "peanut butter and jelly";
+
+const bread = "whole wheat";
+
+const juice = "grape juice";
+
+const fruit = "apple";
+
+
+// Objects
+const lunchObject = {
+    sandwhich: "peanut butter and jelly",
+    bread: "whole wheat",
+    juice: "grape juice",
+    fruit: "apple"
+}
+
+
+
+// Array
+const picnic = [
+    "peanut butter and jelly", "whole wheat", "grape juice"
+];
